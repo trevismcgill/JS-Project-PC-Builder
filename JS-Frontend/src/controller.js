0e3f161ec2dc6e710d0b22150413577e97c0e583
trevismcgill <trevismcgill25@gmail.com>
2020-11-16 20:12:31 -0600

    Added a render method to controller

--- JS-Frontend/src/controller.js
+++ JS-Frontend/src/controller.js
@@ -1,23 +1,36 @@
 class Controller {
     // pcs = []
-    // parts = []
+    static parts = []
     // categories = []
     apiUrl = "http://localhost:3000"
     getCategories() {
         fetch(`${this.apiUrl}/categories`)
         .then(resp => resp.json())
-        .then(data => console.log(data))
+        // .then(data => console.log(data))
         .catch(err => alert(err))
     }
 
     getParts() {
         fetch(`${this.apiUrl}/parts`)
         .then(resp => resp.json())
-        .then(data => data.forEach(part => {
-            Part.all.push(new Part(part.name, part.category.name)
-        )}))
-        .then(console.log(Part.all))
+        .then(data => {
+            data.forEach(part => {
+                Controller.parts.push(new Part(part.name, part.category.name))
+            })
+            this.renderParts()
+            })
         .catch(err => alert(err))
+    }
+
+    renderParts() {
+        let ul = document.createElement('ul')
+        // debugger
+        Controller.parts.forEach(part => {
+            let li = document.createElement('li');
+            li.innerText = part.name
+            ul.appendChild(li)
+        })
+        document.body.appendChild(ul)
     }
 
     // getPc() {
--- JS-Frontend/src/index.js
+++ JS-Frontend/src/index.js
@@ -1,9 +1,8 @@
-console.log("LOADED")
 
 const brain = new Controller
-console.log(brain)
 
 brain.getCategories()
 brain.getParts()
+
 // console.log(Part.all)
 // brain.getPc()
